98e35c123ce1e4cef40c0160b883b7a7eb6b8282
Maurice Ben-Oduro <moduro@iocreate.local>
2016-08-12 17:44:18 -0400

    Finalized auto constraints for elements. Connected elements. Started adding logic.

--- modern-calculator/ViewController.swift
+++ modern-calculator/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+   
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equal = "="
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var calculationLabel: UILabel!
+    @IBOutlet weak var resultsLabel: UILabel!
 
+    var buttonSound: AVAudioPlayer!
+    var runningNumber = ""
+    var rightValueNumber = ""
+    var leftValueNumber = ""
+    var currentOperation = Operation.Empty
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("button", ofType: "wav")
+        
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        // Error Handling
+        do{
+          try buttonSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            buttonSound.prepareToPlay()
+        } catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func numberPressed(btn: UIButton!){
+        
+        buttonSound.play()
+        
+        runningNumber += "\(btn.tag)"
+        calculationLabel.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onSubstractPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+    }
 
+    @IBAction func onEqualPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject){
+        
+        buttonSound.play()
+        
+        calculationLabel.text = "0"
+    }
 
 }
 
